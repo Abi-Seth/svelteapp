3b6169a71bc4660cfa0f3806f9ac132c9db40c60
abi-seth <abiheloaf@gmail.com>
2021-07-29 15:19:51 +0200

    New: conditional stylings

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,15 +1,35 @@
 <script>
     let showModal = true;
+    let isPromo = true;
 </script>
 
-{#if showModal == true}
-    <div>
-        <div>
+{#if showModal}
+    <div class="backdrop" class:promo={isPromo}>
+        <div class="modal">
             <p>Signup options</p>
         </div>
     </div>
 {/if}
 
 <style>
+    .backdrop {
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background: rgba(0,0,0,0.8);
+    }
+
+    .modal {
+        padding: 10px;
+        border-radius: 10px;
+        max-width: 400px;
+        margin: 10% auto;
+        text-align: center;
+    }
+
+    .promo .modal {
+        background: crimson;
+        color: white;
+    }
 
 </style>
