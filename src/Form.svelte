3a98f1f5cb580722d796a71440149771f6dee9cd
abi-seth <abiheloaf@gmail.com>
2021-07-30 20:58:37 +0200

    New: done dealing with checkbox

--- src/Form.svelte
+++ src/Form.svelte
@@ -3,8 +3,14 @@
     let beltColor;
     let age;
 
+    let fighting = false;
+    let running = false;
+    let sneaking = false;
+
+    let skills = [];
+
     const handleForms = () => {
-        console.warn(name, beltColor, age);
+        console.warn(name, beltColor, age, fighting, running, sneaking, skills);
     }
 </script>
 
@@ -12,6 +18,15 @@
     <input type="text" placeholder="Enter name" bind:value={name}>
     <input type="text" placeholder="Enter belt color" bind:value={beltColor}>
     <input type="number" placeholder="Enter age" bind:value={age}>
+    <h4>Skills:</h4>
+    <input type="checkbox" bind:checked={fighting}>Fighting <br>
+    <input type="checkbox" bind:checked={running}>Running <br>
+    <input type="checkbox" bind:checked={sneaking}>Sneaking <br>
+    <p>other way.....</p>
+    <input type="checkbox" bind:group={skills} value="fighting">Fighting <br>
+    <input type="checkbox" bind:group={skills} value="running">Running <br>
+    <input type="checkbox" bind:group={skills} value="sneaking">Sneaking <br>
+    
     <button>Add person</button>
 </form>
 
